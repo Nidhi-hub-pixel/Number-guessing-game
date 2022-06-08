ec22b485f4ca7fe1f1ce16eccbd949f0137a026a
Nidhi-hub-pixel <76686078+Nidhi-hub-pixel@users.noreply.github.com>
2022-06-08 05:16:02 -0700

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,20 @@
+import random
+
+num = random.randint(0, 10)
+print('Number:',num)
+attempt = 4
+msg = 'You Lost!'   
+
+while attempt > 1:
+    user_input = int(input('Enter Number: '))
+
+    if user_input == num:
+        msg = 'You Won!'
+        break
+    else:
+        print('Try again! {attempt} attempt left.')
+        attempt -= 1
+        continue
+
+print(msg)
+
